ff8c5d2342d2f5c165fa8e1034ae698b38d57289
BigB-man <93968558+BigB-man@users.noreply.github.com>
2023-07-25 21:33:14 +1000

    Final
    tis done

--- src/Stock.java
+++ src/Stock.java
@@ -1,11 +1,11 @@
 //Stock stock1 = new Stock("AAPL", "Apple Inc.", 150.25);
 
 public class Stock {
-    private String symbol;
-    private String name;
-    private float price;
+    public String symbol;
+    public String name;
+    public double price;
 
-    public Stock(String symbol, String name, float price)
+    public Stock(String symbol, String name, double price)
     {
         this.symbol = symbol;
         this.name = name;
--- src/StockPortfolio.java
+++ src/StockPortfolio.java
@@ -1,11 +1,17 @@
+import java.util.HashMap; // import the HashMap class
+
 public class StockPortfolio {
-    
-    public StockPortfolio()
+    HashMap<Stock, Integer> portfolio = new HashMap<Stock, Integer>();
+
+    public void addStock(Stock name, int number)
     {
-        
+        this.portfolio.put(name, number);
     }
-    addStock()
+    public void viewPortfolio()
     {
-        
+        for (Stock i : portfolio.keySet())
+        {
+            System.out.println(i.name + "(" + i.symbol + "), Price of stock = $"+ i.price + ", Number of shares = " + portfolio.get(i));
+        }
     }
 }
